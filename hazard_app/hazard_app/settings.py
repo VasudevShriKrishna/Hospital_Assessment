e140294b741626dddade59247c38cfd681c74821
Krish Sangani <rocket.20.launcher18@gmail.com>
2026-02-19 11:09:52 +0530

    updated

--- hazard_app/hazard_app/settings.py
+++ hazard_app/hazard_app/settings.py
@@ -82,8 +82,14 @@ WSGI_APPLICATION = 'hazard_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'livebird',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        }
     }
 }
 
@@ -123,3 +129,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
