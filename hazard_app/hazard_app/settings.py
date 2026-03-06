7688d77382bd8c4b957ae6db606af81ac426c3fc
Krish Sangani <rocket.20.launcher18@gmail.com>
2026-03-06 17:04:12 +0530

    updated

--- hazard_app/hazard_app/settings.py
+++ hazard_app/hazard_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xeok)6q&q*z61yql9v^m!zw6&x!a#u7q4o%ppd#4zp#rr1(vhh'
+SECRET_KEY = 'USE YOUR OWN DJANGO KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 
 
